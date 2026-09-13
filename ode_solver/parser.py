e21cab1e335c00d084d0a33af518146b10634ad6
Alex Jose <alex.lf.jose18@gmail.com>
2026-09-13 16:42:44 +0100

    implemented regex to make a better parser

--- ode_solver/parser.py
+++ ode_solver/parser.py
@@ -1,49 +1,72 @@
+import re
 import sympy as sp
-
+ 
 # example input y' + 2y = e^x
-
+ 
 def parse_ode(equation):
     equation = equation.replace(" ", "")
     print(equation)
-
+ 
     left, right = split_equation(equation)
-
+ 
     parsed_left = 0
-    for expression in left.split("+"):
+    for expression in split_terms(left):
         print(expression)
         if "'" in expression:
             parsed_left += parse_derivative(expression)
         else:
             parsed_left += parse_expression(expression)
-
+ 
     parsed_right = 0
-    for expression in right.split("+"):
+    for expression in split_terms(right):
         if "'" in expression:
             parsed_right += parse_derivative(expression)
         else:
             parsed_right += parse_expression(expression)
-
+ 
     return sp.Eq(parsed_left, parsed_right)
-
-
+ 
+ 
 def split_equation(equation):
     split_equation = equation.split("=")
     print(split_equation)
     return (split_equation[0], split_equation[1])
 
+
+def split_terms(expression):
+    return re.findall(r'[+-]?[^+-]+', expression)
+
 def parse_expression(expression):
-    x = sp.symbols('x')
-    y = sp.Function('y')
     expression = expression.replace("^", "**")
-    return sp.sympify(expression)
+    return parse_coefficient(expression)
+
+
+def parse_coefficient(text):
+    if text in ("", "+"):
+        return 1
+    if text == "-":
+        return -1
+    text = re.sub(r'(\d)([a-zA-Z])', r'\1*\2', text)
+    return sp.sympify(text)
+ 
+ 
+def strip_derivative(term):
+    order = term.count("'")
+    coefficient_part = term.replace("'", "")
+    coefficient_part = coefficient_part.replace("y", "")
+    coefficient_part = coefficient_part.replace("*", "")
+    return coefficient_part, order
+ 
 
 def parse_derivative(derivative):
     x = sp.symbols('x')
     y = sp.Function('y')
-
-    order = derivative.count("'")
-    parse_derivative = sp.diff(y(x), x, order)
-    return parse_derivative
+ 
+    coefficient_part, order = strip_derivative(derivative)
+    deriv_expr = sp.diff(y(x), x, order)
+    coefficient = parse_coefficient(coefficient_part)
+ 
+    return coefficient * deriv_expr
 
 
 ode = "y' + 2*y = e^x"
