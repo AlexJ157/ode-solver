0b3f0c1e21b4aca7f5c6174893b56c536cd6ab37
Alex Jose <alex.lf.jose18@gmail.com>
2026-09-15 21:49:24 +0100

    updated the intergrating factor solver to make it more robust for future UIs

--- ode_solver/analytical.py
+++ ode_solver/analytical.py
@@ -6,34 +6,34 @@ y = sp.Function('y')
 
 def intergrating_factor(rhs):
     is_linear, data = classifier.is_linear(rhs)
-
+ 
     if not is_linear:
-        print("ODE cant be solved using the intergrating factor")
-        return
-
-    P = data.get("P")
-    Q = data.get("Q")
-
-    ode = sp.Eq((sp.diff(y(x), x, 1) + P*y(x)), Q)
-
-    print(f"ODE: y'(x) + {P}y(x) = {Q}\n")
-    print(f"P(x) = {P}, Q(x) = {Q}\n")
-
+        print("ODE cant be solved using the intergrating factor.")
+        return None, []
+ 
+    P, Q = data["p"], data["Q"]
+ 
+    steps = []
+ 
+    ode = sp.Eq(sp.diff(y(x), x, 1) + P * y(x), Q)
+    steps.append(("Standard form", f"y'(x) + {P}y(x) = {Q}"))
+    steps.append(("Identified P, Q", f"P(x) = {P}, Q(x) = {Q}"))
+ 
     integral = sp.integrate(P, x)
     I_factor = sp.exp(integral)
+    steps.append(("Integrating factor I(x)", I_factor))
+ 
     new_lhs = ode.lhs * I_factor
     new_rhs = ode.rhs * I_factor
     new_ode = sp.Eq(new_lhs, new_rhs)
-    print(f"Intergrating factor - I(x): {I_factor}\n")
-    print(f"New ode:  y'(x){I_factor} + {P*I_factor}y(x) = {Q*I_factor}\n")
-    print(f"=> d/dx({I_factor*y(x)}) = {Q*I_factor}\n")
-
-    solution_lhs = I_factor*y(x)
+    steps.append(("Multiply through by I(x)", f"{new_lhs} = {new_rhs}"))
+ 
+    steps.append(("Collapses to", f"d/dx({I_factor * y(x)}) = {new_rhs}"))
+ 
+    solution_lhs = I_factor * y(x)
     solution_rhs = sp.integrate(new_ode.rhs, x)
+    steps.append(("Integrate both sides", f"{solution_lhs} = {solution_rhs} + C"))
+ 
     solution = sp.Eq(solution_lhs, solution_rhs)
-    print(f"Solution: {solution_lhs} = {solution_rhs} + C")
-
-    return solution
-
-
+    return solution, steps
 
